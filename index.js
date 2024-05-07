a77c460ddc2b5ca7a057d8b6a4516db927234451
Gabriel Kramer Mota <120067217+Kramergg@users.noreply.github.com>
2024-05-07 17:25:52 +0000

    Endpoint `Read All [GET] /personagens` implementado

--- index.js
+++ index.js
@@ -5,4 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+//Endpoint Read All [GET] /personagens
+const personagens  = ['Homem Aranha', 'Homem de Ferro', 'Super Homem', 'Goku']
+app.get("/personagens", (req, res) =>{
+    res.send(personagens)
+})
 app.listen(3000)
