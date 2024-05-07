ec4082f015df648fec50cea9b809e74226b79ebc
Gabriel Kramer Mota <120067217+Kramergg@users.noreply.github.com>
2024-05-07 17:42:28 +0000

    Endpoint Read By ID `[GET] /personagens/:id`

--- index.js
+++ index.js
@@ -7,7 +7,19 @@ app.get('/', function (req, res) {
 
 //Endpoint Read All [GET] /personagens
 const personagens  = ['Homem Aranha', 'Homem de Ferro', 'Super Homem', 'Goku']
+
 app.get("/personagens", (req, res) =>{
     res.send(personagens)
 })
-app.listen(3000)
+
+//Endpoint Read by ID [GET] /personagens/:id
+app.get("/personagens/:id", (req, res) => {
+    // Acessamos o parametro de rota ID
+    const id = req.params.id
+    // Acessa o item da lista personagem usando o ID - 1
+    const item = personagens[id - 1]
+    // Enviamos o item como resposta
+    res.send(item)
+})
+
+app.listen(3000, () => 'servidor rodando na porta 3000')
